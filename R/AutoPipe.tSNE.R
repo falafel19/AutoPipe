d7c8a0ca07dfb701cba98c7c8c8ae9816266b0ef
Dieter Henrik Heiland <dieter.henrik.heiland@unilkinik-freiburg.de>
2018-07-01 23:04:26 +0200

    New Functions

---
+++ R/AutoPipe.tSNE.R
@@ -0,0 +1,35 @@
+#' Implemented t-distributed stochastic neighbor embedding 
+#'
+#' This function is used to upload a table into R for further use in the AutoPipe
+#'
+#'
+#' @usage AutoPipe.tSNE(me,perplexity=30,max_iter=500)
+#' 
+#' @param me The path of the expression table
+#' @param perplexity numeric; Perplexity parameter
+#' @param max_iter integer; Number of iterations (default: 1000)
+#'
+#' @export AutoPipe.tSNE
+
+
+
+
+AutoPipe.tSNE=function(me,perplexity=30,max_iter=500){
+  set.seed(5000)
+  ana=Rtsne::Rtsne(t(me), check_duplicates = F, dim=3, perplexity=perplexity, max_iter=max_iter)
+  ana=data.frame(ana$Y)
+  rownames(ana)=colnames(me)
+  ana$cluster=groups_men[rownames(ana), ]$cluster
+  max=max(groups_men[rownames(ana), ]$cluster)
+  ana$col="red"
+  col<-RColorBrewer::brewer.pal(n=max,name = "Paired")
+  for(i in 1:max){
+  color=col[i]
+  ana[ana$cluster==i, ]$col=color
+  
+  }
+  
+  
+  plot(ana[,1:2], col=ana$col, pch=19, bty="n", main="t-distributed stochastic neighbor embedding")
+  
+}
---
+++ R/read.expression.file.R
@@ -0,0 +1,63 @@
+#' Input Expression File 
+#'
+#' This function is used to upload a table into R for further use in the AutoPipe
+#'
+#'
+#' @usage read.expression.file(file, format="csv", sep";",gene_name="SYMBOL",Trans=F)
+#' 
+#' @param file The path of the expression table
+#' @param format The format of the table "csv" or "txt"
+#' @param sep The seperator of the input table
+#' @param gene_name Genes are given in "SYMBOL" or "ENTREZID"
+#' @param Trans Need Matrix Transpose TRUE or FALSE
+#' 
+
+#'
+#' @export read.expression.file
+
+read.expression.file=function(file, format = "csv", sep=";",gene_name="SYMBOL", Trans=F){
+  
+  print(paste("----- Start Import --------"))
+  
+  require(org.Hs.eg.db)
+  
+  data_out=if(format=="csv"){
+    
+    data_out=utils::read.csv(file,row.names=1,header=T,sep = sep )
+    if(Trans==T){data_out=t(data_out)}
+    data_out=if(gene_name=="SYMBOL"){
+      new=clusterProfiler::bitr(rownames(data_out), fromType="SYMBOL", toType="ENTREZID", OrgDb="org.Hs.eg.db")
+      rownames(new)=new$ENTREZID
+      new=new[!duplicated(new$ENTREZID), ]
+      data_out=data_out[new$SYMBOL, ]
+      rownames(data_out)=new$ENTREZID
+      return(data_out)
+    }
+    
+    return(data_out)
+  }
+  data_out=if(format=="txt"){
+    data_out=utils::read.table(file,row.names=1,header=T)
+    if(Trans==T){data_out=(t(data_out))}
+    
+    data_out=if(gene_name=="SYMBOL"){
+      new=clusterProfiler::bitr(rownames(data_out), fromType="SYMBOL", toType="ENTREZID", OrgDb="org.Hs.eg.db")
+      #Remove Duplicated genes
+      new=new[!duplicated(new$ENTREZID), ]
+      
+      rownames(new)=new$ENTREZID
+      
+      data_out=data_out[new$SYMBOL, ]
+      rownames(data_out)=new$ENTREZID
+      return(data_out)
+    }
+    return(data_out)
+  }
+  
+  
+  print(paste("-- Data contained", nrow(data_out), "Genes ------------- Data contained", ncol(data_out), "Samples -----"))
+  
+  return(data_out)
+}
+
+
