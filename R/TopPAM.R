be67ae90c6ea37acff32b7ea17cb22c4c578c37c
Thinkpad-Windows <k.dacca@gmail.com>
2018-12-20 17:54:26 +0100

    Bootstraping option deactivation in TopPam

--- R/TopPAM.R
+++ R/TopPAM.R
@@ -3,7 +3,7 @@
 #' This function computes the n=TOP genes and the the best number of clusters
 #'
 #' @export TopPAM
-#' @usage TopPAM(me, max_clusters=15,TOP=1000,B=100)
+#' @usage TopPAM(me, max_clusters=15,TOP=1000,B=100,clusterboot=FALSE)
 #' @param me a matrix with genes in rows and samples in columns
 #' @param max_clusters max. number of clusters to check
 #' @param TOP the number of genes to take.
@@ -20,13 +20,13 @@
 #' #' ## load data
 #' data(rna)
 #' me_x=rna
-#' res<-AutoPipe::TopPAM(me_x,max_clusters = 8, TOP=100)
+#' res<-AutoPipe::TopPAM(me_x,max_clusters = 8, TOP=100,clusterboot=FALSE)
 #' me_TOP=res[[1]]
 #' number_of_k=res[[3]]
 #' }
 #' @import  graphics
 #' @import  cluster
-TopPAM=function(me, max_clusters=15,TOP=1000,B=100){
+TopPAM=function(me, max_clusters=15,TOP=1000,B=100,clusterboot=FALSE){
   #Top 1000
   dim(me)
   sd=as.data.frame(apply(me,1, function(x){sd(x)}))
@@ -38,18 +38,20 @@ TopPAM=function(me, max_clusters=15,TOP=1000,B=100){
   #How many clusters
   sil_mean=as.numeric(do.call(cbind, lapply(2:max_clusters, function(i){
     pamx <- cluster::pam(t(me_TOP), k=i)
+    print(paste("Cluster with k=",i, sep=""))
     si <- cluster::silhouette(pamx)
     mean_s=mean(as.numeric(si[,3]))
     return(mean_s)
   })))
   
-  
-  
-  
-  gap_st<-cluster::clusGap(t(me_TOP),FUNcluster = cluster::pam,K.max = max_clusters,B=100)
-  graphics::plot(gap_st,main = "Gap Statistics", bty="n",xaxt="n")
-  graphics::axis(side = 1,at = c(1:19),labels= c(2:20))
-  best_nc_gp<-cluster::maxSE(gap_st$Tab[,3],gap_st$Tab[,4],method = "Tibs2001SEmax",.25)
-  
+  gap_st=NULL
+  best_nc_gp=NULL
+  if(clusterboot==TRUE){
+    gap_st<-cluster::clusGap(t(me_TOP),FUNcluster = cluster::pam,K.max = max_clusters,B=100)
+    graphics::plot(gap_st,main = "Gap Statistics", bty="n",xaxt="n")
+    graphics::axis(side = 1,at = c(1:19),labels= c(2:20))
+    best_nc_gp<-cluster::maxSE(gap_st$Tab[,3],gap_st$Tab[,4],method = "Tibs2001SEmax",.25)
+  }
+
   return(list(me_TOP,sil_mean,which.max(sil_mean)+1,gap_st,best_nc_gp))
 }
